0bc60796b27e04d28dfbe130700917e01234021e
aswinmohanme <dev.aswinmohan@gmail.com>
2020-03-29 23:42:06 +0530

    Get Info from Bot

--- bot.rb
+++ bot.rb
@@ -1,7 +1,22 @@
 require 'sinatra/base'
 
 class CoronaChat < Sinatra::Base
-  get '/' do
-    'Hello Corona'
+  use Rack::TwilioWebhookAuthentication, ENV['TWILIO_AUTH_TOKEN'], '/bot'
+
+  post '/bot' do
+    body = params['Body'].downcase
+    response = Twilio::TwiML::MessagingResponse.new
+
+    response.message do |message|
+      case body
+      when 'info'
+        message.body("Hey you entered info")
+      else
+        message.body("Sorry! The command you entered is not recognized, please try entering **menu** or **info**")
+      end
+    end
+
+    content_type "text/xml"
+    response.to_xml
   end
 end
